ec5d77d3853584744b909d9a9588fb8803fc0efe
GuraGaoGao <72219253+GuraGaoGao@users.noreply.github.com>
2023-10-01 16:08:27 +0300

    Update soyblocker.js

--- soyblocker.js
+++ soyblocker.js
@@ -4,7 +4,7 @@ for (let i = 0; i < posts.length; i++) {
 	postChildren = posts[i].getElementsByTagName("div");
 	for (let j = 0; j<postChildren.length; j++) {
 		filePost = postChildren.item(j)
-		if (filePost.className === "fileText" && filePost.textContent.includes("SoyBooru")) {
+		if (filePost.textContent.includes("SoyBooru")) {
 				console.log("soy found!11, engaging lethal weapons")
 				posts[i].style.display = "none"
 		}
